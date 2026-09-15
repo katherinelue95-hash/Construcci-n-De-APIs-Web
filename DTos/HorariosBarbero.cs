3f34580bb5fbb7c40d6694e0279f6ad22c0618eb
Katherinelue <katherinelue95@gmail.com>
2026-09-15 16:12:18 -0600

    Subiendo cambios en DTos

---
+++ DTos/HorariosBarbero.cs
@@ -0,0 +1,11 @@
+﻿namespace StyleBookBarberBD.DTos
+{
+    public class HorariosBarberoDTos
+    {
+        public int HorarioId { get; set; }
+        public int BarberoId { get; set; }
+        public int DiaSemana { get; set; }   // 1=Lunes, 2=Martes, etc.
+        public string HoraInicio { get; set; } = string.Empty; // formato "08:00"
+        public string HoraFin { get; set; } = string.Empty;    // formato "17:00"
+    }
+}
